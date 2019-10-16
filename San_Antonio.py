c414e59a186a863eff56945ae408ec45f61549e1
Houche Serge <houcheserge@gmail.com>
2019-10-16 18:33:22 +0200

    ajout du fichier programme en Python

---
+++ San_Antonio.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
