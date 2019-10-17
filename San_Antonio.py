6d52710e4187658c2ccc007229f196b8f7bf1a78
Houche Serge <houcheserge@gmail.com>
2019-10-17 18:45:22 +0200

    ajout boucle

--- San_Antonio.py
+++ San_Antonio.py
@@ -13,3 +13,23 @@ characters = [
     "le chat potté", 
     "Kirikou"
 ]
+user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
+
+
+
+def get_random_item_in(my_list):
+    # get a random number
+    item = my_list[0] # get a quote from a list
+    return item # return the item
+
+
+
+while user_answer != "B":
+    print(get_random_item_in(quotes))
+    user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
+
+for character in characters:
+  n_character = character.capitalize()
+  print(n_character)
+print(get_random_item_in(quotes))
+# show random quotes
