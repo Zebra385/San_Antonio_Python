70976637f9595f577f14ff32224e2cbfbcbee7b4
Houche Serge <houcheserge@gmail.com>
2019-10-18 13:05:16 +0200

    Utilisation des modules random et json

--- San_Antonio.py
+++ San_Antonio.py
@@ -1,35 +1,35 @@
 # -*- coding: utf8 -*-
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
+import random #import module random
+import json #import module json
 
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
+def get_random_item_in(my_list):#fonction random to choise a character in my list
+    rand_numb = random.randint(0, len(my_list) - 1)#rand_numb have random value between the biginin (0) and the end (len of my list)
+    item = my_list[rand_numb] # get a quote from a list
+    return item # return the item# get a random number
+	
+def read_values_from_json(file, key):
+    values = []#create a list
+    with open(file) as f:# open a json file with my objects
+        data = json.load(f) # load all the data contained in this file f
+        for entry in data:# Create a new empty list
+            values.append(entry[key])# add each item in my list
+    return values # return my completed list   
+	
+def random_character():# Return a random value from a json file
+    all_values = read_values_from_json("characters.json","character")
+    return get_random_item_in(all_values)
+	
+def random_quote():# Return a random value from a json file qotes.json
+    all_values = read_values_from_json("quotes.json","quote")
+    return get_random_item_in(all_values)
+	
+def message(character,quote): #Fonction to create the message
+	return "{} a dit : {} ".format(character,quote)	
+    
 user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
 
 
 
-def get_random_item_in(my_list):
-    # get a random number
-    item = my_list[0] # get a quote from a list
-    return item # return the item
-
-
-
-while user_answer != "B":
-    print(get_random_item_in(quotes))
+while user_answer != "B":# while the answer is differente to "B"
+    print(message(random_character(),random_quote()))
     user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
-
-for character in characters:
-  n_character = character.capitalize()
-  print(n_character)
-print(get_random_item_in(quotes))
-# show random quotes
